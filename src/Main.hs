579ec110b078e98e07e1c5ce38c333ea08d8cd1b
kubni <nikola.kuburovic123@gmail.com>
2024-11-10 12:35:23 +0100

    Implement Assign operation parser for int and str

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Text.Parsec (letter, many1, space, string)
+import Text.Parsec
 import qualified Text.Parsec as Parsec
 import Text.Parsec.String (Parser)
 
@@ -12,8 +12,17 @@ data Statement = Print Var | Assign Var Value deriving (Eq, Show)
 
 data Value = IntVal Int | StringVal String deriving (Eq, Show)
 
+intValParser :: Parser Value
+intValParser = IntVal <$> (read <$> many1 digit)
+
+stringValParser :: Parser Value
+stringValParser = StringVal <$> many1 letter
+
 printParser :: Parser Statement
 printParser = Print <$> (string "print" >> many1 space >> many1 letter)
+
+assignParser :: Parser Statement
+assignParser = Assign <$> (many1 letter) <*> (many1 space >> char '=' >> many1 space >> (try intValParser <|> stringValParser))
 
 main :: IO ()
 main = do
