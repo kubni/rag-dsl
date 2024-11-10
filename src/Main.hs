2e2c2ea0913792ac64a17b264707ee4e175cc66a
kubni <nikola.kuburovic123@gmail.com>
2024-11-10 12:10:51 +0100

    feat: Implement a Print parser

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,20 @@
 module Main where
 
+import Text.Parsec (letter, many1, space, string)
 import qualified Text.Parsec as Parsec
-import Text.Parsec hiding (parse, runParser)
+import Text.Parsec.String (Parser)
+
+type Code = [Statement]
+
+type Var = String
+
+data Statement = Print Var | Assign Var Value deriving (Eq, Show)
+
+data Value = IntVal Int | StringVal String deriving (Eq, Show)
+
+printParser :: Parser Statement
+printParser = Print <$> (string "print" >> many1 space >> many1 letter)
 
 main :: IO ()
 main = do
-  print "Hello cruel world"
+  print "Hello world"
