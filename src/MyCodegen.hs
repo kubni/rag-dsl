bd48efb6384b0433f56d83e7b4ad4c2cfafa6094
kubni <nikola.kuburovic123@gmail.com>
2025-01-07 12:27:06 +0100

    Add necessary imports and funcs for every program
    This isn't the final form for this probably.

--- src/MyCodegen.hs
+++ src/MyCodegen.hs
@@ -18,3 +18,18 @@ statementToPython :: Statement -> String
 statementToPython statement = case statement of
   Assign var value -> var ++ " = " ++ valueToPython value
   Print var -> "print(" ++ var ++ ")"
+
+-- Hardcoded imports that are used by every program we make
+-- Minio is not necessarily required in 100% of cases, but it makes for a better storage of huge payloads that come with base64 data, for which qdrant wasn't optimized.
+-- It also prevents occasional timeouts that Qdrant experiences in such cases.
+generateNecessaryImports :: [String]
+generateNecessaryImports = ["from colpali_engine.models import ColQwen2, ColQwen2Processor", "from pdf2image import convert_from_path", "from minio import Minio", "import base64", "import torch"]
+
+-- TODO: Handle \t and \n inside of the strings
+generateNecessarySetups :: [String]
+generateNecessarySetups =
+  [ "def setup_colqwen(model_name: str, device: str) -> Tuple[PreTrainedModel, Colqwen2ProcessorAliasType]:",
+    "\tcolqwen_model = ColQwen2.from_pretrained(pretrained_model_name_or_path=model_name,torch_dtype=torch.bfloat16,device_map=device)",
+    "\tcolqwen_processor = ColQwen2Processor.from_pretrained(pretrained_model_name_or_path=model_name)",
+    "\treturn colqwen_model, colqwen_processor"
+  ]
